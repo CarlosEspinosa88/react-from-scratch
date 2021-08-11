c085383acec271ec9836d75ccdb92f444b253cc4
Carlos Espinosa <carloespinosa88@gmail.com>
2021-08-11 11:06:57 -0500

    feat: config app and html webpack plugin

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import imageExp from '../images/email.png';
+
+export default function Home() {
+  return (
+    <>
+      <p>Hola estoy en Home</p>
+      <img src={imageExp} alt="email-ayenda" width="300" />
+    </>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.jsx"
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'React from Scratch'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+  resolve: { 
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.SourceMapDevToolPlugin({})
+  ],
+};
