8e375af9a9d31db6d29a8c4b9b119c57d69952c1
Carlos Espinosa <carloespinosa88@gmail.com>
2022-05-03 11:25:45 -0500

    feat: add testing in button and counter, also add types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function Button({ title, ...props }: { title: string }) {
-  return <button {...props}>{title}</button>;
+export type ButtonType = {
+  title: string;
+  onPress: () => void;
+};
+
+export default function Button({ title, onPress, ...props }: ButtonType) {
+  return (
+    <button onClick={onPress} {...props}>
+      {title}
+    </button>
+  );
 }
